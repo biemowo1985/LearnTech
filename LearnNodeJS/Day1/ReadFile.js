d78d43cfd4cbf6a2dd7d072016a52ef88ef21e54
unknown <biemowo1985@hotmail.com>
2018-11-10 21:14:43 +0800

    Delete the "WriteFile" method in readfile.js

--- LearnNodeJS/Day1/ReadFile.js
+++ LearnNodeJS/Day1/ReadFile.js
@@ -7,12 +7,3 @@ fs.readFile('../Files/ReadFile.txt', function (error, data) {
         console.log(data.toString())
     }
 })
-
-fs.writeFile('../Files/WriteFile.txt', 'This is the write file data', function (err) {
-    if (err) {
-        console.log(err.message)
-    }
-    else {
-        console.log('Write file successful!')
-    }
-})

9957bd4b7eb496714633dd27b194f9fd0948f818
Kevin Yang <biemowo1985@gmail.com>
2018-11-20 16:45:49 +0800

    add ';' to the line end

--- LearnNodeJS/Day1/ReadFile.js
+++ LearnNodeJS/Day1/ReadFile.js
@@ -1,9 +1,9 @@
-var fs = require('fs')
+var fs = require('fs');
 fs.readFile('../Files/ReadFile.txt', function (error, data) {
     if (error) {
-        console.log(error.message)
+        console.log(error.message);
     }
     else {
-        console.log(data.toString())
+        console.log(data.toString());
     }
-})
+});
--- LearnNodeJS/Day1/WriteFile.js
+++ LearnNodeJS/Day1/WriteFile.js
@@ -1,10 +1,10 @@
-var fs = require('fs')
+var fs = require('fs');
 
 fs.writeFile('../Files/WriteFile.txt', 'This is the write file data', function (err) {
     if (err) {
-        console.log(err.message)
+        console.log(err.message);
     }
     else {
-        console.log('Write file successful!')
+        console.log('Write file successful!');
     }
-})
+});

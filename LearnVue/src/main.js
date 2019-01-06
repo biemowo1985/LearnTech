0b0229a3c5d84383e5cf1f959c7f859d0636d7b0
Kevin Yang <biemowo1985@hotmail.com>
2019-01-06 19:33:24 +0800

    upload the todolist project

--- LearnVue/src/main.js
+++ LearnVue/src/main.js
@@ -1,8 +1,12 @@
 /*jshint esversion:6 */
 import Vue from 'vue';
 import App from './App.vue';
+import Todo from './sessions/Todolist.vue'
+// import './assets/bootstrap/css/bootstrap.min.css'
+// import './assets/bootstrap/js/bootstrap.min'
+
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+  el: '#todo',
+  render: h => h(Todo)
 });
---
+++ LearnVue/src/sessions/Todolist.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="todo">
+        <h1>
+            {{msg}}
+        </h1>
+        <button class="btn btn-success">success</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "todo",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  },
+  methods: {
+    getModel() {
+      alert(this.msg);
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+
+</style>

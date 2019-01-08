69e8309d453bd5e8aa6220b10db0c745a8af6632
Kevin Yang <kyang@touchpointmed.com>
2019-01-08 16:26:06 +0800

    upload the session07 source code and update the readme file

--- LearnVue/README.md
+++ LearnVue/README.md
@@ -1,18 +1,3 @@
-# learn_vue
-
-> A Vue.js project
-
-## Build Setup
-
-``` bash
-# install dependencies
-npm install
-
-# serve with hot reload at localhost:8080
-npm run dev
-
-# build for production with minification
-npm run build
-```
-
-For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
+# Session07 TodoList
+    Javascript 数组 -- push方法：向数组中添加记录
+                    -- splice方法：从数组中删除记录
--- LearnVue/src/sessions/Todolist.vue
+++ LearnVue/src/sessions/Todolist.vue
@@ -1,10 +1,23 @@
 <template>
-    <div id="todo">
-        <h1>
-            {{msg}}
-        </h1>
-        <button class="btn btn-success">success</button>
+  <div id="todo">
+    <div>
+      <h1>{{msg}}</h1>
+      <button class="btn btn-success">success</button>
     </div>
+    <br>
+    <div>
+      <input type="text" v-model="todoItem">
+      <button @click="addItems">Add</button>
+    </div>
+    <div>
+      <ul>
+        <li v-for="(value, key) in todoList" v-bind:key="key">
+          {{value}} -----
+          <button @click="deleteItem(key)">Delete</button>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -12,17 +25,27 @@ export default {
   name: "todo",
   data() {
     return {
-      msg: "Welcome to Your Vue.js App"
+      msg: "Welcome to Your Vue.js App",
+      todoItem: "",
+      todoList: []
     };
   },
   methods: {
     getModel() {
       alert(this.msg);
+    },
+    addItems() {
+      this.todoList.push(this.todoItem);
+      this.todoItem = "";
+    },
+    deleteItem(key) {
+      if (this.todoList.length > key) {
+        this.todoList.splice(key, 1);
+      }
     }
   }
 };
 </script>
 
 <style lang="scss">
-
 </style>

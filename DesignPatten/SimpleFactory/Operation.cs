8c261487d71e73976bd0a0c8ee73261796e15b9a
kyang <kyang@XIA-KYANG-LT>
2019-05-22 14:42:09 +0800

    complete the simple factory pattern

---
+++ DesignPatten/SimpleFactory/Operation.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactory
+{
+    public class Operation
+    {
+        public double NumberA { get; set; }
+        public double NumberB { get; set; }
+        public virtual double Calculate()
+        {
+            double result = 0;
+            return result;
+        }
+    }
+
+    public class OperationAdd : Operation
+    {
+        public override double Calculate()
+        {
+            return NumberA + NumberB;
+        }
+    }
+
+    public class OperationSub : Operation
+    {
+        public override double Calculate()
+        {
+            return NumberA - NumberB;
+        }
+    }
+
+    public class OperationMul : Operation
+    {
+        public override double Calculate()
+        {
+            return NumberA * NumberB;
+        }
+    }
+
+    public class OperationDiv : Operation
+    {
+        public override double Calculate()
+        {
+            if (NumberB == 0)
+            {
+                throw new Exception("Attempted to divide by zero.");
+            }
+            return NumberA / NumberB;
+        }
+    }
+}
---
+++ DesignPatten/SimpleFactory/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var oper = OperationFactory.CreateOperation("+");
+            oper.NumberA = 3;
+            oper.NumberB = 5;
+            var result = oper.Calculate();
+
+            Console.WriteLine("Calculate Result is: " + result);
+            Console.ReadLine();
+        }
+    }
+}

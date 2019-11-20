247ef74b3444bd8e2e99f5028370b0ead45afdeb
Kevin Yang <kyang@touchpointmed.com>
2019-11-20 15:28:04 +0800

    update the source code of session03

--- koa_lesson03/async_await.js
+++ koa_lesson03/async_await.js
@@ -1,3 +1,6 @@
+/*jshint esversion: 8 */
+
+/* ----------------- 以前的写法 ------------------ */
 //普通方法
 function getData() {
     return 'Kevin';
@@ -17,3 +20,20 @@ var p = getDataAsync();
 p.then((data) => {
     console.log(data);
 });
+
+/* ----------------- Koa里最新的写法 ------------------ */
+function getData() {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            var username = 'Kevin';
+            res(username);
+        }, 1000);
+    });
+}
+
+async function test() {
+    var d = await getData();
+    console.log(d);
+}
+
+test();
